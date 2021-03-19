097e9039a845666a359e2adb1f718eadf84b62ed
danielevergani <dvergani94@gmail.com>
2021-03-19 16:12:26 +0100

    snack metodo correzione

--- es 1/js/script.js
+++ es 1/js/script.js
@@ -1,4 +1,33 @@
-var bici = [
+// var bici = [
+//     {
+//         nome: "graziella",
+//         peso: 4000
+//     },
+//     {
+//         nome: "bianchi",
+//         peso: 2000,
+//     },
+//     {
+//         nome: "pininfarina",
+//         peso: 5000,
+//     }
+// ];
+
+// var {nome, peso} = bici[0];
+
+
+// for ( let i = 0; i < bici.length; i++){
+//     if(peso > bici[i].peso){
+//         peso = bici[i].peso
+//         nome = bici[i].nome
+//     }
+    
+// }
+
+// console.log( `la bici che pesa di meno è quella della ${nome} ` );
+
+
+const bici = [
     {
         nome: "graziella",
         peso: 4000
@@ -13,15 +42,14 @@ var bici = [
     }
 ];
 
-var {nome, peso} = bici[0];
-
+let biciLight = null;
 
 for ( let i = 0; i < bici.length; i++){
-    if(peso > bici[i].peso){
-        peso = bici[i].peso
-        nome = bici[i].nome
+    if (biciLight == null || biciLight.peso > bici[i].peso ){
+        biciLight = bici[i]
     }
-    
 }
+
+const {nome, peso} = biciLight;
 
 console.log( `la bici che pesa di meno è quella della ${nome} ` );
