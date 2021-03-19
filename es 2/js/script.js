bde5c7a46c4df9f808d29d4bdcc113d38f3167a5
danielevergani <dvergani94@gmail.com>
2021-03-19 16:29:29 +0100

    snack 2 mio metodo

---
+++ es 2/js/script.js
@@ -0,0 +1,44 @@
+const squadre = [
+    {
+        nome: "inter",
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: "roma",
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: "samp",
+        punti: 0,
+        falli: 0
+    }
+];
+
+
+for ( let i = 0; i <squadre.length; i++){
+    var {punti, falli} = squadre[i];
+    punti = Math.floor(Math.random() * 100) + 1;;
+    falli = Math.floor(Math.random() * 10) + 1;
+    squadre[i].punti = punti;
+    squadre[i].falli = falli;
+}
+
+console.log(squadre);
+
+let squadre2 = [];
+let singolaSquadra
+
+for ( let i = 0; i <squadre.length; i++){
+    var {nome, falli} = squadre[i];
+
+    singolaSquadra = {
+        nome,
+        falli,
+    };
+
+    squadre2.push(singolaSquadra);
+}
+
+console.log(squadre2);
